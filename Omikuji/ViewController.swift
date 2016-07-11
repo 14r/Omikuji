4c1aae98d85f64a9b37e74b871db272f183cfd61
14r <73marly@gmail.com>
2016-07-11 16:55:08 +0900

    追加課題

--- Omikuji/ViewController.swift
+++ Omikuji/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var label : UILabel!
+    
+    var omikujiArray: [UILabel]!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func gacha(){
+        
+        let number = Int(rand() % 10)
+        
+        var omikujiArray : [String] = ["大吉",
+                                       "中吉",
+                                       "中吉",
+                                       "吉",
+                                       "吉",
+                                       "吉",
+                                       "小吉",
+                                       "小吉",
+                                       "小吉",
+                                       "凶"
+        ]
+        
+        label.text = omikujiArray[number]
+        
+        if number == 0 {
+            label.textColor = UIColor.redColor()
+        } else {
+            label.textColor = UIColor.yellowColor()
+        }
+        
+    }
+    
 }
-
